2e18ac7817687448e5896cfe25a8a5302ea6d37d
ACA30 <ndbroughton6@gmail.com>
2020-02-12 08:58:23 -0500

    Dog Treats

---
+++ j1-dogtreats.py
@@ -0,0 +1,14 @@
+# 1 × S + 2 × M + 3 × L
+
+s = int(input())
+m = int(input())
+l = int(input())
+
+total = 1 * s + 2 * m + 3 * l
+
+if total >= 10:
+    print("happy")
+
+else:
+    print("sad")
+    
